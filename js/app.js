293b7698b2e319cf1e348149b3cd77e4fcf81be1
NoobSammy <aldana.samuel.aprogram@gmail.com>
2023-01-09 18:18:55 -0600

    feat: estilos para enlaces activos del footer
    - Cuando el mouse esta sobre los iconos de redes sociales, se
      modifica su src para mostrar el icono en version blanca.
    - Cuando el mouse se pasa a otro icono o sale de ellos, se devuelve su
      src inicial.

--- js/app.js
+++ js/app.js
@@ -5,4 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
 	menuBtn.addEventListener("click", () => {
 		menu.classList.toggle("active");
 	});
+
+	const footerSocial = document.querySelector(".footer__social");
+	const images = footerSocial.querySelectorAll(".footer__icon img");
+	const resetImages = () =>
+		images.forEach((img) => (img.src = img.src.replace("-white", "")));
+
+	footerSocial.addEventListener("mouseover", (e) => {
+		if (e.target.matches(".footer__icon img")) {
+			resetImages();
+			const icon = e.target.dataset.icon;
+			e.target.src = `images/icon-${icon}-white.svg`;
+		}
+	});
+	footerSocial.addEventListener("mouseleave", (e) => resetImages());
 });
